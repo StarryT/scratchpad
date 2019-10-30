d507c9fb737af18748d5889651c06776f20171bd
starryT <1481356738@qq.com>
2019-10-30 21:08:59 +0800

    2019.10.30 晚上9:08

---
+++ bin/docs/数据库/scratchpad.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE `scratch` CHARSET UTF8;
+
+USE `scratch`;
+
+CREATE TABLE `user`(
+`username` VARCHAR(32) PRIMARY KEY,
+`password` VARCHAR(30) NOT NULL,
+`nickname` VARCHAR(30)  NOT NULL,
+`gender` CHAR(1)  NOT NULL,
+`phone` VARCHAR(30)  NOT NULL,
+`email` VARCHAR(30)  NOT NULL
+)CHARSET  utf8 ENGINE INNODB; 
+
+CREATE TABLE `note`(
+`id` VARCHAR(30) PRIMARY KEY,
+`name` VARCHAR(30)  NOT NULL,
+`content` TEXT ,
+`owner` VARCHAR(30) ,
+FOREIGN KEY(`owner`) REFERENCES user(`username`)
+)CHARSET  utf8 ENGINE INNODB;
